c07fd886e4ef0d5fdcbc34a118ea1d0fed100dcd
jose-almir <jr.cod.dev@gmail.com>
2025-10-04 22:54:50 -0300

    new post 04-10-2025 font loading

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="pt-br">
-      <Head />
+      <Head>
+        <link href="https://fonts.googleapis.com/css2?family=Hanken+Grotesk:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet"/>
+        </Head>
       <body>
         <Main />
         <NextScript />
